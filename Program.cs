fc82bbf783e543934ba47cd273d87f262cecf799
CTH-npitx <chatch@npitx.org>
2025-10-01 10:19:02 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -36,5 +36,6 @@ namespace snackShack
         } //this is for error messages. the text is mandatory, but the
           //rest isn't necessary. if you don't provide an exception, it won't display one. If you provide an error, it will default to "hide error".
           // WARNING: If you provide a boolean, you can controll if it's hidden. If you provide a boolean but no error, well, it won't work righ
+    
     }
 }
