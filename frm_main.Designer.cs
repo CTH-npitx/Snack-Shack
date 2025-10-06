20e82a74b07c75ac40dccef48dea0bf7128fceb4
CTH-npitx <chatch@npitx.org>
2025-10-06 11:04:53 -0500

    Revert "test"
    This reverts commit b38d87c0d75aa456b9a6e52b4253c2aa105a3357.

--- frm_main.Designer.cs
+++ frm_main.Designer.cs
@@ -28,11 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -47,30 +44,13 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // imageList1
-            // 
-            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
-            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // frm_main
+            // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
-            this.Name = "frm_main";
+            this.Name = "Form1";
             this.Text = "Main Form";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -81,8 +61,6 @@
 
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
