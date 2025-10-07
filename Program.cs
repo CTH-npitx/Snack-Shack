c722d6f9b44a322cd97c2c20fdbd92ae0fff7b76
CTH-npitx <chatch@npitx.org>
2025-10-07 11:06:15 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ namespace snackShack
         } //this is for error messages. the text is mandatory, but the
           //rest isn't necessary. if you don't provide an exception, it won't display one. If you provide an error, it will default to "hide error".
           // WARNING: If you provide a boolean, you can controll if it's hidden. If you provide a boolean but no error, well, it won't work righ
-    
+   
         internal static string path()
         {
             var pathSpliter = '\\'; //part between paths
