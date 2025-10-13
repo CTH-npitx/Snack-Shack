63c58ac122f96b543fade30fa95c9bda558aacb8
CTH-npitx <chatch@npitx.org>
2025-10-13 11:38:39 -0500

    move premade error message

--- constants.cs
+++ constants.cs
@@ -3,6 +3,7 @@ namespace snackShack.files
     internal static class constants
     {
         const char fileSepChar_con = '\\';
+        const string preMadeErrorMsg = "I'm sorry dave, I'm afraid I can't do that"; //pre-made error message
         internal static string fileSepString()
         {
             return char.ToString(fileSepChar_con);
