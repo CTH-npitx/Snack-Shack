b043ae1c7301e3566e4fc096bd22d7dd0c8892bb
CTH-npitx <chatch@npitx.org>
2025-10-21 10:56:21 -0500

    click on image box

--- frm_main.Designer.cs
+++ frm_main.Designer.cs
@@ -139,14 +139,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(476, 213);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // frm_main
             // 
