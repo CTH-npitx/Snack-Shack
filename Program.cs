60b6723a64fd75a89825249a6f440a8e29f3dc25
CTH-npitx <chatch@npitx.org>
2025-11-03 10:56:09 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace snackShack
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
             Application.EnableVisualStyles();
