84f6e44144ed32945d457f84456fd85598d0b6f3
CTH-npitx <chatch@npitx.org>
2025-11-06 11:06:55 -0600

    Update constants.cs

--- constants.cs
+++ constants.cs
@@ -2,16 +2,16 @@ namespace snackShack
 {
     internal static class constants //pre-defined constants
     {
-        const char fileSepChar_con = '\\';
         internal const int min = 4;
         internal const char entrySep = ',';
+        const char fileSepChar_con = '\\'; //seperater in file paths
         internal const string preMadeErrorMsg = "I'm sorry dave, I'm afraid I can't do that"; //pre-made error message
 
-        internal static string fileSepString()
+        internal static string fileSepString() //give the seperation character as a string
         {
             return char.ToString(fileSepChar_con);
         }
-        internal static char fileSepChar()
+        internal static char fileSepChar() //give the seperation character as a char
         {
             return fileSepChar_con;
         }
