14b36cfb6f591d7f090bd59bde2ba18ccd47fa5a
CTH-npitx <chatch@npitx.org>
2025-11-06 11:13:54 -0600

    Update frm_main.cs

--- frm_main.cs
+++ frm_main.cs
@@ -77,6 +77,7 @@ namespace snackShack
         }
 
         private void picBox_icon_Click(object sender, EventArgs e) //click on image input
+        private void btn_appClose(object sender, EventArgs e)
         {
             string imagePath; //the path
             openFileDialog1.Filter = "All Files (*.*)|*.*|JPG (*.jpg*)|*.jpg"; //allow them to sort for a specific extension (the one utilized by this program), or any file
@@ -85,6 +86,7 @@ namespace snackShack
                 imagePath = openFileDialog1.FileName; //set the path
             }
             
+            Application.Exit();
         }
 
     }
