2a9708b09069d731835e983306c0c6af12e1e430
CTH-npitx <chatch@npitx.org>
2025-11-10 10:27:53 -0600

    Update frm_main.Designer.cs

--- frm_main.Designer.cs
+++ frm_main.Designer.cs
@@ -103,6 +103,7 @@
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
             this.closeToolStripMenuItem.ShowShortcutKeys = false;
             this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
             this.closeToolStripMenuItem.Text = "Close";
