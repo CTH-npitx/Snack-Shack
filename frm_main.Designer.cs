bf8cebf74ba562db373c48361de0e74aa46b6407
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-11-24 11:39:14 -0600

    Update frm_main.Designer.cs

--- frm_main.Designer.cs
+++ frm_main.Designer.cs
@@ -47,12 +47,14 @@
             this.col_snaName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dgv_invent = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.num_snackCost)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBox_icon)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_invent)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -118,7 +120,7 @@
             0,
             0,
             131072});
-            this.num_snackCost.Location = new System.Drawing.Point(55, 45);
+            this.num_snackCost.Location = new System.Drawing.Point(47, 66);
             this.num_snackCost.Maximum = new decimal(new int[] {
             10,
             0,
@@ -141,19 +143,16 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Controls.Add(this.txt_nameInput);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.num_snackCost);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(464, 27);
+            this.panel1.Location = new System.Drawing.Point(509, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(336, 398);
+            this.panel1.Size = new System.Drawing.Size(291, 362);
             this.panel1.TabIndex = 6;
             // 
             // txt_nameInput
             // 
-            this.txt_nameInput.Location = new System.Drawing.Point(44, 6);
+            this.txt_nameInput.Location = new System.Drawing.Point(47, 35);
             this.txt_nameInput.Name = "txt_nameInput";
             this.txt_nameInput.Size = new System.Drawing.Size(100, 20);
             this.txt_nameInput.TabIndex = 7;
@@ -161,7 +160,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 6);
+            this.label3.Location = new System.Drawing.Point(6, 38);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 8;
@@ -170,7 +169,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 45);
+            this.label1.Location = new System.Drawing.Point(6, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 6;
@@ -218,12 +217,25 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.picBox_icon);
-            this.groupBox1.Location = new System.Drawing.Point(79, 71);
+            this.groupBox1.Location = new System.Drawing.Point(43, 109);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(245, 259);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Snack Icon";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.num_snackCost);
+            this.groupBox2.Controls.Add(this.txt_nameInput);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(3, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(151, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Snack Information";
             // 
             // frm_main
             // 
@@ -246,9 +258,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.num_snackCost)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picBox_icon)).EndInit();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_invent)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,6 +287,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn col_snaName;
         private System.Windows.Forms.DataGridView dgv_invent;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
