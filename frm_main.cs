90ddd07ac8482067de3d508398c2f016b7549fb2
CTH-npitx <chatch@npitx.org>
2025-12-01 11:52:24 -0600

    retreive extraneus edits

--- frm_main.cs
+++ frm_main.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
-using System.Security.Policy;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,16 +15,9 @@ namespace snackShack
 {
     public partial class frm_main : Form
     {
-        string rootPath = snackShack.coreCommands.path();
-
-        string imageFolder = snackShack.coreCommands.path() + "Files" + constants.fileSepString() + "presetImages" + constants.fileSepString();
-        string inventoryFile = snackShack.coreCommands.path() + "Files" + constants.fileSepString() + nameof(inventory) + ".csv";
-
         public frm_main()
         {
             InitializeComponent();
-            openFileDialog1.InitialDirectory = imageFolder; //set default path
-            readInvent();
         }
         string rootPath = string.Empty;
         string inventoryFile = string.Empty;
@@ -53,64 +45,18 @@ namespace snackShack
             RemoveMenu(hMenu, num_menu_items - 2, MF_BYPOSITION); // Remove Minimise
         }
         #endregion
-
-        }
-
-        private void frmMain_close(object sender, FormClosingEventArgs e)
+        private void frmMain_load(object sender, EventArgs e)
         {
-            closeSystem(true);
-            
+            rootPath = snackShack.coreCommands.path();
+            inventoryFile = rootPath + nameof(inventory) + ".csv";
+            imageFolder = rootPath + "presetImages" + fileSep;
+            removeClose();
         }
 
-        private void btn_appClose(object sender, EventArgs e) //close app system
+        private void tsb_close(object sender, EventArgs e)
         {
-            closeSystem();
+            Application.Exit();
         }
 
-        private void btn_addItem_Click(object sender, EventArgs e) //add item
-        {
-
-        }
-
-        private void picBox_icon_Click(object sender, EventArgs e) //click on image input
-        {
-            string imagePath; //the path
-            openFileDialog1.Filter = "All Files (*.*)|*.*|JPG (*.jpg*)|*.jpg"; //allow them to sort for a specific extension (the one utilized by this program), or any file
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                imagePath = openFileDialog1.FileName; //set the path
-            }
-
-        }
-
-        #region Close System
-        private void close()
-        {
-            bool confirm = false; //make confirm variable, default to false
-            DialogResult result = MessageBox.Show("Are you sure you want to close the application?", "Confirm Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
-            {
-                confirm = true;
-            }
-            if (confirm == true)
-            {
-                Application.Exit(); //close app
-            }
-            else
-            {
-
-            }
-        }
-        private void closeSystem(bool viaMeneu = false)
-        {
-            if (viaMeneu)
-            {
-                saveInvent();
-            } else
-            {
-                close();
-            }
-        }
-        #endregion
     }
 }
