19ff7b592362f6eb5982908f5b33d5f95404f2b9
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2025-12-02 12:07:47 -0600

    Revert "Merge branch 'Remove-X-Button' of https://github.com/CTH-npitx/Snack-Shack into Remove-X-Button"
    This reverts commit aad3b0ad86a53583d6078e75c741b12ef92adf66, reversing
    changes made to 614819c14857b35bc68f91ec337f45da8b379c24.

--- frm_main.cs
+++ frm_main.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization.Configuration;
-
 
 namespace snackShack
 {
@@ -26,32 +25,7 @@ namespace snackShack
             InitializeComponent();
             openFileDialog1.InitialDirectory = imageFolder; //set default path
             readInvent();
-            removeClose();
         }
-        string rootPath = string.Empty;
-        string inventoryFile = string.Empty;
-        string imageFolder = string.Empty;
-        const char fileSepChar = '\\';
-        string fileSep = char.ToString(fileSepChar);
-        //remove close (based on https://csharphelper.com/howtos/howto_remove_close_x.html )
-        #region removeClose
-        // Declare User32 constants and methods.
-        private const int MF_BYPOSITION = 0x400;
-        [DllImport("User32")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-
-        [DllImport("User32")]
-        private static extern int GetMenuItemCount(IntPtr hWnd);
-
-        [DllImport("User32")]
-        private static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
-        // Remove the X button.
-        void removeClose()
-        {
-            IntPtr hMenu = GetSystemMenu(this.Handle, false);
-            int num_menu_items = GetMenuItemCount(hMenu);
-            RemoveMenu(hMenu, num_menu_items - 1, MF_BYPOSITION); // Remove Close
-            RemoveMenu(hMenu, num_menu_items - 2, MF_BYPOSITION); // Remove Minimise
         private void readInvent() //readInvent
         {
             snackShack.files.read(inventoryFile, snackShack.constants.entrySep, snackShack.constants.min); //this is a pre-setup read inventory function. That way reffernecing it is easier
@@ -61,62 +35,8 @@ namespace snackShack
         {
             snackShack.files.Write(inventoryFile, snackShack.constants.entrySep); //this is a pre - setup function for writing files, to make auto - saveing easier
         }
-        string rootPath = string.Empty;
-        string inventoryFile = string.Empty;
-        string imageFolder = string.Empty;
-        const char fileSepChar = '\\';
-        string fileSep = char.ToString(fileSepChar);
-        //remove close (based on https://csharphelper.com/howtos/howto_remove_close_x.html )
-        #region removeClose
-        // Declare User32 constants and methods.
-        private const int MF_BYPOSITION = 0x400;
-        [DllImport("User32")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-
-        [DllImport("User32")]
-        private static extern int GetMenuItemCount(IntPtr hWnd);
-
-        [DllImport("User32")]
-        private static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
-        // Remove the X button.
-        void removeClose()
-        {
-            IntPtr hMenu = GetSystemMenu(this.Handle, false);
-            int num_menu_items = GetMenuItemCount(hMenu);
-            RemoveMenu(hMenu, num_menu_items - 1, MF_BYPOSITION); // Remove Close
-            RemoveMenu(hMenu, num_menu_items - 2, MF_BYPOSITION); // Remove Minimise
-        }
-        #endregion
-        private void frmMain_load(object sender, EventArgs e)
-        {
-            rootPath = snackShack.coreCommands.path();
-            inventoryFile = rootPath + nameof(inventory) + ".csv";
-            imageFolder = rootPath + "presetImages" + fileSep;
-            removeClose();
 
         private void frmMain_load(object sender, EventArgs e)
-        //remove close (based on https://csharphelper.com/howtos/howto_remove_close_x.html )
-        #region removeClose
-        // Declare User32 constants and methods.
-        private const int MF_BYPOSITION = 0x400;
-        [DllImport("User32")]
-        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
-
-        [DllImport("User32")]
-        private static extern int GetMenuItemCount(IntPtr hWnd);
-
-        [DllImport("User32")]
-        private static extern int RemoveMenu(IntPtr hMenu, int nPosition, int wFlags);
-        // Remove the X button.
-        void removeClose()
-        {
-            IntPtr hMenu = GetSystemMenu(this.Handle, false);
-            int num_menu_items = GetMenuItemCount(hMenu);
-            RemoveMenu(hMenu, num_menu_items - 1, MF_BYPOSITION); // Remove Close
-            RemoveMenu(hMenu, num_menu_items - 2, MF_BYPOSITION); // Remove Minimise
-        }
-        #endregion
-
         {
 
         }
@@ -176,7 +96,6 @@ namespace snackShack
                 close();
             }
         }
-
         #endregion
     }
 }
