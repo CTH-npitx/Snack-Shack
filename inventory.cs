e34ffdf2bdc50b9ad478dbab60d498559619ed74
CTH-npitx <chatch@npitx.org>
2026-01-12 11:13:36 -0600

    Revert "Merge branch 'fix-form-2.0_intermediate' into correct-integrate-adress-form"
    This reverts commit f311d8022a9eafad4c60650f8a0932edd515e7db, reversing
    changes made to 92b4541502f76dc1322ebdd2c386f6e83fb50d5f.

---
+++ inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snackShack
+{
+    internal class inventory //the class that stores the inventory
+    {
+        internal string name; //name of snack
+        internal string imagePath; //path of image
+        internal int cost; //cost of snack
+        internal int index; //the index of the entry
+    }
+}
