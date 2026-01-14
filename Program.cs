6c6140ea54eaa5354c4746d1b2ce909997cd54b5
CTH-npitx <chatch@npitx.org>
2026-01-14 11:47:04 -0600

    Revert "t"
    This reverts commit 07cc3ae65e7ce9455c5c92172414abd4683c736d.

--- Program.cs
+++ Program.cs
@@ -38,4 +38,4 @@ namespace AdressBook
           //rest isn't necessary. if you don't provide an exception, it won't display one. If you provide an error, it will default to "hide error".
           // WARNING: If you provide a boolean, you can controll if it's hidden. If you provide a boolean but no error, well, it won't work righ
     }
-} 
+}
