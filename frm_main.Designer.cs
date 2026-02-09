a1efe5d08b44a552c3146521e100865d801bea24
CTH-npitx <chatch@npitx.org>
2026-02-09 10:33:49 -0600

    picture box initial

--- frm_main.Designer.cs
+++ frm_main.Designer.cs
@@ -121,6 +121,8 @@
             // 
             // picBox_icon
             // 
+            this.picBox_icon.Image = global::snackShack.Properties.Resources.VendingMachine;
+            this.picBox_icon.InitialImage = global::snackShack.Properties.Resources.VendingMachine;
             this.picBox_icon.Location = new System.Drawing.Point(6, 19);
             this.picBox_icon.Name = "picBox_icon";
             this.picBox_icon.Size = new System.Drawing.Size(235, 229);
