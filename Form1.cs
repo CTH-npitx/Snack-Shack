bfd75af6b98cfc23c06a10fde33f511700182e04
CTH-npitx <chatch@npitx.org>
2026-03-30 11:20:50 -0500

    if

--- Form1.cs
+++ Form1.cs
@@ -20,6 +20,7 @@ namespace snackShack
         private void bttn_snackImage_Click_1(object sender, EventArgs e)
         {
             openFileDialog1.Filter = "Image Files(*.jpg,*.jpeg,*.png,*.gif,*bmp)|*.jpg,*.jpeg,*.png,*.gif,*.bmp";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) { }
         }
     }
 }
