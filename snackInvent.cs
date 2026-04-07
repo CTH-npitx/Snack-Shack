a364fc0305a87bf94acb8843e291db4086798986
CTH-npitx <chatch@npitx.org>
2026-04-07 11:12:15 -0500

    comment name

--- snackInvent.cs
+++ snackInvent.cs
@@ -8,9 +8,9 @@ namespace snackShack
 {
     internal class snackInvent
     {
-        internal string name;
         internal double price;
         internal int amount;
         internal string imagepath;
+        internal string name; //name of snack
     }
 }
