a99a32340b0dc877018e7a75bf563c4f3e3ae591
CTH-npitx <chatch@npitx.org>
2026-04-09 13:59:27 -0500

    Revert "more currently irelevant stuff"
    This reverts commit 0832487002ddd8a37855c83d20d2b3f10f61b54d.

--- constants.cs
+++ constants.cs
@@ -7,16 +7,14 @@ namespace snackShack
         internal const char entrySep = ','; //seperation character
         internal const string preMadeErrorMsg = "I'm sorry dave, I'm afraid I can't do that"; //pre-made error message
         internal const double minPrice = 0.05;
-        /*internal static string fileSepString() //give the seperation character as a string
+        internal static string fileSepString() //give the seperation character as a string
         {
-            char.ToString(fileSepChar_con);
+            return char.ToString(fileSepChar_con);
         }
-        */
-       /* internal static char fileSepChar() //give the seperation character as a char
+        internal static char fileSepChar() //give the seperation character as a char
         {
             return fileSepChar_con;
         }
-       */
         internal static decimal minimumPrice()
         {
             return (decimal)minPrice;
