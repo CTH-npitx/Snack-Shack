33abc181362dd9d0eb522547f2cd0355264b871e
Um... Figure it out! <33612115+CTH999@users.noreply.github.com>
2026-04-23 14:41:54 -0500

    Update constants.cs

--- constants.cs
+++ constants.cs
@@ -8,6 +8,8 @@ namespace snackShack
         internal const char entrySep = ','; //seperation character
         internal const string preMadeErrorMsg = "I'm sorry dave, I'm afraid I can't do that"; //pre-made error message
         internal const decimal minPrice = 0.05M;
+        internal const string fileName = "snacks";
+        internal const string fileExtension = "csv";
         /* internal static char fileSepChar() //give the seperation character as a char
          {
              return fileSepChar_con;
