667d6cfd214a8dea67e048ef6f68b51ea05aaba7
CTH-npitx <chatch@npitx.org>
2026-04-27 10:05:05 -0500

    Update frm_snackInvent.cs

--- frm_snackInvent.cs
+++ frm_snackInvent.cs
@@ -105,12 +105,10 @@ namespace snackShack
                             string line = sr.ReadLine(); //read line
                             string[] arr = line.Split(constants.entrySep); //split into an array based on the csv format
 
-                            //populate class based on the contents of the file
-                            snack.name = arr[0];
-                            snack.price = double.Parse(arr[1]);
-                            snack.amount = Int32.Parse(arr[2]);
-                            snack.imagepath = arr[3];
-                            snack.index = int.Parse(arr[4]); 
+                            //format values for import (including ensuring values valid)
+                            string nameImport = arr[0];
+                            double.Parse(arr[1]);
+                            double importPrice = double.Parse(arr[2]);
 
                             count++;
                             toolStripStatusLabel1.Text =
