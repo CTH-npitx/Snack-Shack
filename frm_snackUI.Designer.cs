1862524c5b47c78e8c2049bb0a9198645d99534d
CTH-npitx <chatch@npitx.org>
2026-04-29 10:34:19 -0500

    location

--- frm_snackUI.Designer.cs
+++ frm_snackUI.Designer.cs
@@ -32,20 +32,6 @@
             this.pb_snack1 = new System.Windows.Forms.PictureBox();
             this.lbl_snack1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pb_snack2 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack2 = new System.Windows.Forms.Label();
-            this.pb_snack3 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack3 = new System.Windows.Forms.Label();
-            this.pb_snack4 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack4 = new System.Windows.Forms.Label();
-            this.pb_snack8 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack8 = new System.Windows.Forms.Label();
-            this.pb_snack7 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack7 = new System.Windows.Forms.Label();
-            this.pb_snack6 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack6 = new System.Windows.Forms.Label();
-            this.pb_snack5 = new System.Windows.Forms.PictureBox();
-            this.lbl_snack5 = new System.Windows.Forms.Label();
             this.pb_snack12 = new System.Windows.Forms.PictureBox();
             this.lbl_snack12 = new System.Windows.Forms.Label();
             this.pb_snack11 = new System.Windows.Forms.PictureBox();
@@ -54,19 +40,33 @@
             this.lbl_snack10 = new System.Windows.Forms.Label();
             this.pb_snack9 = new System.Windows.Forms.PictureBox();
             this.lbl_snack9 = new System.Windows.Forms.Label();
+            this.pb_snack8 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack8 = new System.Windows.Forms.Label();
+            this.pb_snack7 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack7 = new System.Windows.Forms.Label();
+            this.pb_snack6 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack6 = new System.Windows.Forms.Label();
+            this.pb_snack5 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack5 = new System.Windows.Forms.Label();
+            this.pb_snack4 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack4 = new System.Windows.Forms.Label();
+            this.pb_snack3 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack3 = new System.Windows.Forms.Label();
+            this.pb_snack2 = new System.Windows.Forms.PictureBox();
+            this.lbl_snack2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack1)).BeginInit();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack12)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack11)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).BeginInit();
             this.SuspendLayout();
             // 
             // pb_snack1
@@ -108,83 +108,6 @@
             this.panel1.Controls.Add(this.lbl_snack1);
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            // 
-            // pb_snack2
-            // 
-            resources.ApplyResources(this.pb_snack2, "pb_snack2");
-            this.pb_snack2.Name = "pb_snack2";
-            this.pb_snack2.TabStop = false;
-            // 
-            // lbl_snack2
-            // 
-            resources.ApplyResources(this.lbl_snack2, "lbl_snack2");
-            this.lbl_snack2.Name = "lbl_snack2";
-            // 
-            // pb_snack3
-            // 
-            resources.ApplyResources(this.pb_snack3, "pb_snack3");
-            this.pb_snack3.Name = "pb_snack3";
-            this.pb_snack3.TabStop = false;
-            // 
-            // lbl_snack3
-            // 
-            resources.ApplyResources(this.lbl_snack3, "lbl_snack3");
-            this.lbl_snack3.Name = "lbl_snack3";
-            // 
-            // pb_snack4
-            // 
-            resources.ApplyResources(this.pb_snack4, "pb_snack4");
-            this.pb_snack4.Name = "pb_snack4";
-            this.pb_snack4.TabStop = false;
-            // 
-            // lbl_snack4
-            // 
-            resources.ApplyResources(this.lbl_snack4, "lbl_snack4");
-            this.lbl_snack4.Name = "lbl_snack4";
-            // 
-            // pb_snack8
-            // 
-            resources.ApplyResources(this.pb_snack8, "pb_snack8");
-            this.pb_snack8.Name = "pb_snack8";
-            this.pb_snack8.TabStop = false;
-            // 
-            // lbl_snack8
-            // 
-            resources.ApplyResources(this.lbl_snack8, "lbl_snack8");
-            this.lbl_snack8.Name = "lbl_snack8";
-            // 
-            // pb_snack7
-            // 
-            resources.ApplyResources(this.pb_snack7, "pb_snack7");
-            this.pb_snack7.Name = "pb_snack7";
-            this.pb_snack7.TabStop = false;
-            // 
-            // lbl_snack7
-            // 
-            resources.ApplyResources(this.lbl_snack7, "lbl_snack7");
-            this.lbl_snack7.Name = "lbl_snack7";
-            // 
-            // pb_snack6
-            // 
-            resources.ApplyResources(this.pb_snack6, "pb_snack6");
-            this.pb_snack6.Name = "pb_snack6";
-            this.pb_snack6.TabStop = false;
-            // 
-            // lbl_snack6
-            // 
-            resources.ApplyResources(this.lbl_snack6, "lbl_snack6");
-            this.lbl_snack6.Name = "lbl_snack6";
-            // 
-            // pb_snack5
-            // 
-            resources.ApplyResources(this.pb_snack5, "pb_snack5");
-            this.pb_snack5.Name = "pb_snack5";
-            this.pb_snack5.TabStop = false;
-            // 
-            // lbl_snack5
-            // 
-            resources.ApplyResources(this.lbl_snack5, "lbl_snack5");
-            this.lbl_snack5.Name = "lbl_snack5";
             // 
             // pb_snack12
             // 
@@ -230,6 +153,83 @@
             resources.ApplyResources(this.lbl_snack9, "lbl_snack9");
             this.lbl_snack9.Name = "lbl_snack9";
             // 
+            // pb_snack8
+            // 
+            resources.ApplyResources(this.pb_snack8, "pb_snack8");
+            this.pb_snack8.Name = "pb_snack8";
+            this.pb_snack8.TabStop = false;
+            // 
+            // lbl_snack8
+            // 
+            resources.ApplyResources(this.lbl_snack8, "lbl_snack8");
+            this.lbl_snack8.Name = "lbl_snack8";
+            // 
+            // pb_snack7
+            // 
+            resources.ApplyResources(this.pb_snack7, "pb_snack7");
+            this.pb_snack7.Name = "pb_snack7";
+            this.pb_snack7.TabStop = false;
+            // 
+            // lbl_snack7
+            // 
+            resources.ApplyResources(this.lbl_snack7, "lbl_snack7");
+            this.lbl_snack7.Name = "lbl_snack7";
+            // 
+            // pb_snack6
+            // 
+            resources.ApplyResources(this.pb_snack6, "pb_snack6");
+            this.pb_snack6.Name = "pb_snack6";
+            this.pb_snack6.TabStop = false;
+            // 
+            // lbl_snack6
+            // 
+            resources.ApplyResources(this.lbl_snack6, "lbl_snack6");
+            this.lbl_snack6.Name = "lbl_snack6";
+            // 
+            // pb_snack5
+            // 
+            resources.ApplyResources(this.pb_snack5, "pb_snack5");
+            this.pb_snack5.Name = "pb_snack5";
+            this.pb_snack5.TabStop = false;
+            // 
+            // lbl_snack5
+            // 
+            resources.ApplyResources(this.lbl_snack5, "lbl_snack5");
+            this.lbl_snack5.Name = "lbl_snack5";
+            // 
+            // pb_snack4
+            // 
+            resources.ApplyResources(this.pb_snack4, "pb_snack4");
+            this.pb_snack4.Name = "pb_snack4";
+            this.pb_snack4.TabStop = false;
+            // 
+            // lbl_snack4
+            // 
+            resources.ApplyResources(this.lbl_snack4, "lbl_snack4");
+            this.lbl_snack4.Name = "lbl_snack4";
+            // 
+            // pb_snack3
+            // 
+            resources.ApplyResources(this.pb_snack3, "pb_snack3");
+            this.pb_snack3.Name = "pb_snack3";
+            this.pb_snack3.TabStop = false;
+            // 
+            // lbl_snack3
+            // 
+            resources.ApplyResources(this.lbl_snack3, "lbl_snack3");
+            this.lbl_snack3.Name = "lbl_snack3";
+            // 
+            // pb_snack2
+            // 
+            resources.ApplyResources(this.pb_snack2, "pb_snack2");
+            this.pb_snack2.Name = "pb_snack2";
+            this.pb_snack2.TabStop = false;
+            // 
+            // lbl_snack2
+            // 
+            resources.ApplyResources(this.lbl_snack2, "lbl_snack2");
+            this.lbl_snack2.Name = "lbl_snack2";
+            // 
             // frm_snackUI
             // 
             resources.ApplyResources(this, "$this");
@@ -239,17 +239,17 @@
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack12)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack11)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).EndInit();
             this.ResumeLayout(false);
 
         }
