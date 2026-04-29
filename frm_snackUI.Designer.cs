efa9824e6effbae34715bf19a3544164c11a8bfc
CTH-npitx <chatch@npitx.org>
2026-04-29 10:19:56 -0500

    rename labels

--- frm_snackUI.Designer.cs
+++ frm_snackUI.Designer.cs
@@ -30,30 +30,30 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_snackUI));
             this.pb_snack1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_snack1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pb_snack2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_snack2 = new System.Windows.Forms.Label();
             this.pb_snack3 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_snack3 = new System.Windows.Forms.Label();
             this.pb_snack4 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_snack4 = new System.Windows.Forms.Label();
             this.pb_snack8 = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_snack8 = new System.Windows.Forms.Label();
             this.pb_snack7 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_snack7 = new System.Windows.Forms.Label();
             this.pb_snack6 = new System.Windows.Forms.PictureBox();
-            this.label7 = new System.Windows.Forms.Label();
+            this.lbl_snack6 = new System.Windows.Forms.Label();
             this.pb_snack5 = new System.Windows.Forms.PictureBox();
-            this.label8 = new System.Windows.Forms.Label();
+            this.lbl_snack5 = new System.Windows.Forms.Label();
             this.pb_snack12 = new System.Windows.Forms.PictureBox();
-            this.label9 = new System.Windows.Forms.Label();
+            this.lbl_snack12 = new System.Windows.Forms.Label();
             this.pb_snack11 = new System.Windows.Forms.PictureBox();
-            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_snack11 = new System.Windows.Forms.Label();
             this.pb_snack10 = new System.Windows.Forms.PictureBox();
-            this.label11 = new System.Windows.Forms.Label();
+            this.lbl_snack10 = new System.Windows.Forms.Label();
             this.pb_snack9 = new System.Windows.Forms.PictureBox();
-            this.label12 = new System.Windows.Forms.Label();
+            this.lbl_snack9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).BeginInit();
@@ -75,37 +75,37 @@
             this.pb_snack1.Name = "pb_snack1";
             this.pb_snack1.TabStop = false;
             // 
-            // label1
+            // lbl_snack1
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.lbl_snack1, "lbl_snack1");
+            this.lbl_snack1.Name = "lbl_snack1";
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.pb_snack12);
-            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.lbl_snack12);
             this.panel1.Controls.Add(this.pb_snack11);
-            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.lbl_snack11);
             this.panel1.Controls.Add(this.pb_snack10);
-            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.lbl_snack10);
             this.panel1.Controls.Add(this.pb_snack9);
-            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.lbl_snack9);
             this.panel1.Controls.Add(this.pb_snack8);
-            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lbl_snack8);
             this.panel1.Controls.Add(this.pb_snack7);
-            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lbl_snack7);
             this.panel1.Controls.Add(this.pb_snack6);
-            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.lbl_snack6);
             this.panel1.Controls.Add(this.pb_snack5);
-            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lbl_snack5);
             this.panel1.Controls.Add(this.pb_snack4);
-            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lbl_snack4);
             this.panel1.Controls.Add(this.pb_snack3);
-            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbl_snack3);
             this.panel1.Controls.Add(this.pb_snack2);
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lbl_snack2);
             this.panel1.Controls.Add(this.pb_snack1);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbl_snack1);
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
@@ -115,10 +115,10 @@
             this.pb_snack2.Name = "pb_snack2";
             this.pb_snack2.TabStop = false;
             // 
-            // label2
+            // lbl_snack2
             // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
+            resources.ApplyResources(this.lbl_snack2, "lbl_snack2");
+            this.lbl_snack2.Name = "lbl_snack2";
             // 
             // pb_snack3
             // 
@@ -126,10 +126,10 @@
             this.pb_snack3.Name = "pb_snack3";
             this.pb_snack3.TabStop = false;
             // 
-            // label3
+            // lbl_snack3
             // 
-            resources.ApplyResources(this.label3, "label3");
-            this.label3.Name = "label3";
+            resources.ApplyResources(this.lbl_snack3, "lbl_snack3");
+            this.lbl_snack3.Name = "lbl_snack3";
             // 
             // pb_snack4
             // 
@@ -137,10 +137,10 @@
             this.pb_snack4.Name = "pb_snack4";
             this.pb_snack4.TabStop = false;
             // 
-            // label4
+            // lbl_snack4
             // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Name = "label4";
+            resources.ApplyResources(this.lbl_snack4, "lbl_snack4");
+            this.lbl_snack4.Name = "lbl_snack4";
             // 
             // pb_snack8
             // 
@@ -148,10 +148,10 @@
             this.pb_snack8.Name = "pb_snack8";
             this.pb_snack8.TabStop = false;
             // 
-            // label5
+            // lbl_snack8
             // 
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.Name = "label5";
+            resources.ApplyResources(this.lbl_snack8, "lbl_snack8");
+            this.lbl_snack8.Name = "lbl_snack8";
             // 
             // pb_snack7
             // 
@@ -159,10 +159,10 @@
             this.pb_snack7.Name = "pb_snack7";
             this.pb_snack7.TabStop = false;
             // 
-            // label6
+            // lbl_snack7
             // 
-            resources.ApplyResources(this.label6, "label6");
-            this.label6.Name = "label6";
+            resources.ApplyResources(this.lbl_snack7, "lbl_snack7");
+            this.lbl_snack7.Name = "lbl_snack7";
             // 
             // pb_snack6
             // 
@@ -170,10 +170,10 @@
             this.pb_snack6.Name = "pb_snack6";
             this.pb_snack6.TabStop = false;
             // 
-            // label7
+            // lbl_snack6
             // 
-            resources.ApplyResources(this.label7, "label7");
-            this.label7.Name = "label7";
+            resources.ApplyResources(this.lbl_snack6, "lbl_snack6");
+            this.lbl_snack6.Name = "lbl_snack6";
             // 
             // pb_snack5
             // 
@@ -181,10 +181,10 @@
             this.pb_snack5.Name = "pb_snack5";
             this.pb_snack5.TabStop = false;
             // 
-            // label8
+            // lbl_snack5
             // 
-            resources.ApplyResources(this.label8, "label8");
-            this.label8.Name = "label8";
+            resources.ApplyResources(this.lbl_snack5, "lbl_snack5");
+            this.lbl_snack5.Name = "lbl_snack5";
             // 
             // pb_snack12
             // 
@@ -192,10 +192,10 @@
             this.pb_snack12.Name = "pb_snack12";
             this.pb_snack12.TabStop = false;
             // 
-            // label9
+            // lbl_snack12
             // 
-            resources.ApplyResources(this.label9, "label9");
-            this.label9.Name = "label9";
+            resources.ApplyResources(this.lbl_snack12, "lbl_snack12");
+            this.lbl_snack12.Name = "lbl_snack12";
             // 
             // pb_snack11
             // 
@@ -203,10 +203,10 @@
             this.pb_snack11.Name = "pb_snack11";
             this.pb_snack11.TabStop = false;
             // 
-            // label10
+            // lbl_snack11
             // 
-            resources.ApplyResources(this.label10, "label10");
-            this.label10.Name = "label10";
+            resources.ApplyResources(this.lbl_snack11, "lbl_snack11");
+            this.lbl_snack11.Name = "lbl_snack11";
             // 
             // pb_snack10
             // 
@@ -214,10 +214,10 @@
             this.pb_snack10.Name = "pb_snack10";
             this.pb_snack10.TabStop = false;
             // 
-            // label11
+            // lbl_snack10
             // 
-            resources.ApplyResources(this.label11, "label11");
-            this.label11.Name = "label11";
+            resources.ApplyResources(this.lbl_snack10, "lbl_snack10");
+            this.lbl_snack10.Name = "lbl_snack10";
             // 
             // pb_snack9
             // 
@@ -225,10 +225,10 @@
             this.pb_snack9.Name = "pb_snack9";
             this.pb_snack9.TabStop = false;
             // 
-            // label12
+            // lbl_snack9
             // 
-            resources.ApplyResources(this.label12, "label12");
-            this.label12.Name = "label12";
+            resources.ApplyResources(this.lbl_snack9, "lbl_snack9");
+            this.lbl_snack9.Name = "lbl_snack9";
             // 
             // frm_snackUI
             // 
@@ -257,29 +257,29 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pb_snack1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_snack1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pb_snack12;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lbl_snack12;
         private System.Windows.Forms.PictureBox pb_snack11;
-        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl_snack11;
         private System.Windows.Forms.PictureBox pb_snack10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lbl_snack10;
         private System.Windows.Forms.PictureBox pb_snack9;
-        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lbl_snack9;
         private System.Windows.Forms.PictureBox pb_snack8;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_snack8;
         private System.Windows.Forms.PictureBox pb_snack7;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_snack7;
         private System.Windows.Forms.PictureBox pb_snack6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_snack6;
         private System.Windows.Forms.PictureBox pb_snack5;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lbl_snack5;
         private System.Windows.Forms.PictureBox pb_snack4;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_snack4;
         private System.Windows.Forms.PictureBox pb_snack3;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_snack3;
         private System.Windows.Forms.PictureBox pb_snack2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_snack2;
     }
 }
