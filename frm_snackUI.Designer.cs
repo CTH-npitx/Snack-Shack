994f55c447bdfe6972e7fc86ccc4d415d9ce82e9
CTH-npitx <chatch@npitx.org>
2026-04-29 10:16:13 -0500

    rename picture boxes

--- frm_snackUI.Designer.cs
+++ frm_snackUI.Designer.cs
@@ -29,51 +29,51 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_snackUI));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pb_snack1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pb_snack2 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pb_snack3 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pb_snack4 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pb_snack8 = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pb_snack7 = new System.Windows.Forms.PictureBox();
             this.label6 = new System.Windows.Forms.Label();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pb_snack6 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pb_snack5 = new System.Windows.Forms.PictureBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pb_snack12 = new System.Windows.Forms.PictureBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pb_snack11 = new System.Windows.Forms.PictureBox();
             this.label10 = new System.Windows.Forms.Label();
-            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pb_snack10 = new System.Windows.Forms.PictureBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pb_snack9 = new System.Windows.Forms.PictureBox();
             this.label12 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack1)).BeginInit();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack9)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pb_snack1
             // 
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.TabStop = false;
+            resources.ApplyResources(this.pb_snack1, "pb_snack1");
+            this.pb_snack1.Name = "pb_snack1";
+            this.pb_snack1.TabStop = false;
             // 
             // label1
             // 
@@ -82,148 +82,148 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.pictureBox9);
+            this.panel1.Controls.Add(this.pb_snack12);
             this.panel1.Controls.Add(this.label9);
-            this.panel1.Controls.Add(this.pictureBox10);
+            this.panel1.Controls.Add(this.pb_snack11);
             this.panel1.Controls.Add(this.label10);
-            this.panel1.Controls.Add(this.pictureBox11);
+            this.panel1.Controls.Add(this.pb_snack10);
             this.panel1.Controls.Add(this.label11);
-            this.panel1.Controls.Add(this.pictureBox12);
+            this.panel1.Controls.Add(this.pb_snack9);
             this.panel1.Controls.Add(this.label12);
-            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pb_snack8);
             this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pb_snack7);
             this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pb_snack6);
             this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.pictureBox8);
+            this.panel1.Controls.Add(this.pb_snack5);
             this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pb_snack4);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pb_snack3);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pb_snack2);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pb_snack1);
             this.panel1.Controls.Add(this.label1);
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
-            // pictureBox2
+            // pb_snack2
             // 
-            resources.ApplyResources(this.pictureBox2, "pictureBox2");
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.TabStop = false;
+            resources.ApplyResources(this.pb_snack2, "pb_snack2");
+            this.pb_snack2.Name = "pb_snack2";
+            this.pb_snack2.TabStop = false;
             // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
             // 
-            // pictureBox3
+            // pb_snack3
             // 
-            resources.ApplyResources(this.pictureBox3, "pictureBox3");
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.TabStop = false;
+            resources.ApplyResources(this.pb_snack3, "pb_snack3");
+            this.pb_snack3.Name = "pb_snack3";
+            this.pb_snack3.TabStop = false;
             // 
             // label3
             // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
             // 
-            // pictureBox4
+            // pb_snack4
             // 
-            resources.ApplyResources(this.pictureBox4, "pictureBox4");
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.TabStop = false;
+            resources.ApplyResources(this.pb_snack4, "pb_snack4");
+            this.pb_snack4.Name = "pb_snack4";
+            this.pb_snack4.TabStop = false;
             // 
             // label4
             // 
             resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
             // 
-            // pictureBox5
+            // pb_snack8
             // 
-            resources.ApplyResources(this.pictureBox5, "pictureBox5");
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.TabStop = false;
+            resources.ApplyResources(this.pb_snack8, "pb_snack8");
+            this.pb_snack8.Name = "pb_snack8";
+            this.pb_snack8.TabStop = false;
             // 
             // label5
             // 
             resources.ApplyResources(this.label5, "label5");
             this.label5.Name = "label5";
             // 
-            // pictureBox6
+            // pb_snack7
             // 
-            resources.ApplyResources(this.pictureBox6, "pictureBox6");
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.TabStop = false;
+            resources.ApplyResources(this.pb_snack7, "pb_snack7");
+            this.pb_snack7.Name = "pb_snack7";
+            this.pb_snack7.TabStop = false;
             // 
             // label6
             // 
             resources.ApplyResources(this.label6, "label6");
             this.label6.Name = "label6";
             // 
-            // pictureBox7
+            // pb_snack6
             // 
-            resources.ApplyResources(this.pictureBox7, "pictureBox7");
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.TabStop = false;
+            resources.ApplyResources(this.pb_snack6, "pb_snack6");
+            this.pb_snack6.Name = "pb_snack6";
+            this.pb_snack6.TabStop = false;
             // 
             // label7
             // 
             resources.ApplyResources(this.label7, "label7");
             this.label7.Name = "label7";
             // 
-            // pictureBox8
+            // pb_snack5
             // 
-            resources.ApplyResources(this.pictureBox8, "pictureBox8");
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.TabStop = false;
+            resources.ApplyResources(this.pb_snack5, "pb_snack5");
+            this.pb_snack5.Name = "pb_snack5";
+            this.pb_snack5.TabStop = false;
             // 
             // label8
             // 
             resources.ApplyResources(this.label8, "label8");
             this.label8.Name = "label8";
             // 
-            // pictureBox9
+            // pb_snack12
             // 
-            resources.ApplyResources(this.pictureBox9, "pictureBox9");
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.TabStop = false;
+            resources.ApplyResources(this.pb_snack12, "pb_snack12");
+            this.pb_snack12.Name = "pb_snack12";
+            this.pb_snack12.TabStop = false;
             // 
             // label9
             // 
             resources.ApplyResources(this.label9, "label9");
             this.label9.Name = "label9";
             // 
-            // pictureBox10
+            // pb_snack11
             // 
-            resources.ApplyResources(this.pictureBox10, "pictureBox10");
-            this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.TabStop = false;
+            resources.ApplyResources(this.pb_snack11, "pb_snack11");
+            this.pb_snack11.Name = "pb_snack11";
+            this.pb_snack11.TabStop = false;
             // 
             // label10
             // 
             resources.ApplyResources(this.label10, "label10");
             this.label10.Name = "label10";
             // 
-            // pictureBox11
+            // pb_snack10
             // 
-            resources.ApplyResources(this.pictureBox11, "pictureBox11");
-            this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.TabStop = false;
+            resources.ApplyResources(this.pb_snack10, "pb_snack10");
+            this.pb_snack10.Name = "pb_snack10";
+            this.pb_snack10.TabStop = false;
             // 
             // label11
             // 
             resources.ApplyResources(this.label11, "label11");
             this.label11.Name = "label11";
             // 
-            // pictureBox12
+            // pb_snack9
             // 
-            resources.ApplyResources(this.pictureBox12, "pictureBox12");
-            this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.TabStop = false;
+            resources.ApplyResources(this.pb_snack9, "pb_snack9");
+            this.pb_snack9.Name = "pb_snack9";
+            this.pb_snack9.TabStop = false;
             // 
             // label12
             // 
@@ -236,50 +236,50 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
             this.Name = "frm_snackUI";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_snack9)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pb_snack1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pb_snack12;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pb_snack11;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pb_snack10;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pb_snack9;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pb_snack8;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pb_snack7;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pb_snack6;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pb_snack5;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pb_snack4;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pb_snack3;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pb_snack2;
         private System.Windows.Forms.Label label2;
     }
 }
