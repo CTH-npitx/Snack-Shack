ec4d17e8472cdcfb163954bc7155ed22975a311e
CTH-npitx <chatch@npitx.org>
2026-05-05 12:10:35 -0500

    vending machine image

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace snackShack.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap VendingMachine1 {
+            get {
+                object obj = ResourceManager.GetObject("VendingMachine1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
